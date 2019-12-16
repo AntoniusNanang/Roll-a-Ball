295d39f32acd1603fdf3240895997077fb39130a
AntoniusNanang <nkcgta.antonius.nanang@gmail.com>
2019-12-16 14:38:14 +0900

    マップ調整

---
+++ Roll a ball/Assets/Script/FollowGyro.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGyro : MonoBehaviour
+{
+    [Header("Tweaks")]
+    [SerializeField] private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+    float Limit = 15.0f;         // 回転の限界値
+    float x, y, z;               // それぞれの回転を記憶する値
+    // Start is called before the first frame update
+    void Start()
+    {
+        GyroManager.Instance.EnableGyro();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = (GyroManager.Instance.GetGyroRotation());
+        
+    }
+
+    //private bool isLimit()
+    //{
+    //    if() { return false; }
+    //    return true;
+    //}
+}
---
+++ Roll a ball/Assets/Script/GyroManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroManager : MonoBehaviour
+{
+    #region Instance
+    private static GyroManager instance;
+    public static GyroManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<GyroManager>();
+                if(instance == null)
+                {
+                    instance = new GameObject("Spawned,GyroManager", typeof(GyroManager)).GetComponent<GyroManager>();
+                }
+            }
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+
+    [Header("Logic")]
+    private Gyroscope gyro;
+    private Quaternion rotation;
+    private bool gyroActive;
+    private float limitRot = 0.3f;     // 回転の限界値±0.5
+
+    public void EnableGyro()
+    {
+        // Already activated
+        if (gyroActive) return;
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            gyroActive = gyro.enabled;
+        }
+        else
+        {
+            Debug.Log("Gyro is not supported on this device");
+        }
+    }
+
+    void Update()
+    {
+        if (gyroActive)
+        {  
+            rotation = gyro.attitude;
+            Debug.Log(rotation);
+        }
+    }
+
+    public Quaternion GetGyroRotation()
+    {
+        float t;
+        t = rotation.y;
+        rotation.y = rotation.z*-1;
+        rotation.z = t;
+        rotation.w *= 0;
+
+        if (rotation.x >= limitRot) rotation.x = 0.3f;
+        if (rotation.x <= limitRot * -1) rotation.x = -0.3f;
+
+        return rotation;
+    }
+}
+
+
